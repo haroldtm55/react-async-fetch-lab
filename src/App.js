0688c98e1332c2f4f19938df1aebcc1a0c3e17af
Harold Torres Marino <haroldtm55@gmail.com>
2021-07-15 16:18:25 +1000

    Lab completed

--- src/App.js
+++ src/App.js
@@ -1,1 +1,29 @@
 // create your App component here
+import React, {Component} from 'react'
+
+export default class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      peopleInSpace: []
+    }
+  }
+
+  componentDidMount() {
+    fetch('http://api.open-notify.org/astros.json')
+      .then(resp => resp.json())
+      .then(astros => {
+        this.setState({
+          peopleInSpace: astros.people
+        })
+    })
+  }
+
+  render() {
+    return (
+      <ul>
+        {this.state.peopleInSpace.map((astro,idx)=><li key={idx}>{astro.name}</li>)}
+      </ul>
+    )
+  }
+}
